f64cc7bd6a036759c2b057e9ab93eb9137a3f918
Adam Kowalewski <adam.kowalewski@student.put.poznan.pl>
2023-05-21 19:19:11 +0200

    Init code commit

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CompanyApi.Models;
+
+namespace CompanyApi.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly CompanyDbContext _context;
+
+        public DepartmentsController(CompanyDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: Departments
+        [HttpGet(Name = "GetDepartments")]
+        public async Task<IActionResult> Index()
+        {
+              return _context.Departments != null ? 
+                          View(await _context.Departments.ToListAsync()) :
+                          Problem("Entity set 'CompanyDbContext.Departments'  is null.");
+        }
+
+        // GET: Departments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Departments == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments
+                .FirstOrDefaultAsync(m => m.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // GET: Departments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DepartmentId,Name")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(department);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Departments == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Departments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DepartmentId,Name")] Department department)
+        {
+            if (id != department.DepartmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(department);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartmentExists(department.DepartmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Departments == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments
+                .FirstOrDefaultAsync(m => m.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // POST: Departments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Departments == null)
+            {
+                return Problem("Entity set 'CompanyDbContext.Departments'  is null.");
+            }
+            var department = await _context.Departments.FindAsync(id);
+            if (department != null)
+            {
+                _context.Departments.Remove(department);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartmentExists(int id)
+        {
+          return (_context.Departments?.Any(e => e.DepartmentId == id)).GetValueOrDefault();
+        }
+
+        private static DepartmentDTO DepartmentToDTO(Department department) =>
+            new DepartmentDTO
+            {
+                DepartmentId = department.DepartmentId,
+                Name = department.Name,
+            };
+
+        private static Department DTOToDepartment(DepartmentDTO departmentDTO) =>
+            new Department
+            {
+                DepartmentId = departmentDTO.DepartmentId,
+                Name = departmentDTO.Name,
+            };
+
+        [HttpGet("Department")]
+        public async Task<ActionResult<IEnumerable<DepartmentDTO>>> GetDepartment()
+        {
+            return await _context.Departments
+                .Select(e => DepartmentToDTO(e))
+                .ToListAsync();
+        }
+
+        [HttpGet("Department/{id}")]
+        public async Task<ActionResult<DepartmentDTO>> GetDepartment(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return DepartmentToDTO(department);
+        }
+
+        [HttpPut("Department/{id}")]
+        public async Task<IActionResult> PutDepartment(int id, DepartmentDTO departmentDTO)
+        {
+            if (id != departmentDTO.DepartmentId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(DTOToDepartment(departmentDTO)).State =
+                EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpPost("Department")]
+        public async Task<ActionResult<DepartmentDTO>> PostDepartmet(DepartmentDTO departmentDTO)
+        {
+            _context.Departments.Add(DTOToDepartment(departmentDTO));
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetDepartment",
+                new { id = departmentDTO.DepartmentId }, departmentDTO);
+        }
+
+        [HttpDelete("Department/{id}")]
+        public async Task<ActionResult<DepartmentDTO>> DeleteDepartment(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+            return DepartmentToDTO(department);
+        }
+    }
+}
---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CompanyApi.Models;
+
+namespace CompanyApi.Controllers
+{
+
+    public class EmployeesController : Controller
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmployeesController(CompanyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var companyDbContext = _context.Employees.Include(e => e.Department).Include(e => e.Manager);
+            return View(await companyDbContext.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Department)
+                .Include(e => e.Manager)
+                .FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId");
+            ViewData["ManagerId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmployeeId,FirstName,LastName,ManagerId,Salary,Bonus,DepartmentId")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", employee.DepartmentId);
+            ViewData["ManagerId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", employee.ManagerId);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", employee.DepartmentId);
+            ViewData["ManagerId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", employee.ManagerId);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,FirstName,LastName,ManagerId,Salary,Bonus,DepartmentId")] Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.EmployeeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", employee.DepartmentId);
+            ViewData["ManagerId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", employee.ManagerId);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Department)
+                .Include(e => e.Manager)
+                .FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Employees == null)
+            {
+                return Problem("Entity set 'CompanyDbContext.Employees'  is null.");
+            }
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+          return (_context.Employees?.Any(e => e.EmployeeId == id)).GetValueOrDefault();
+        }
+
+        private static EmployeeDTO EmployeeToDTO(Employee employee) =>
+            new EmployeeDTO 
+            { 
+                EmployeeId = employee.EmployeeId,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                ManagerId = employee.ManagerId,
+                Salary = employee.Salary
+                , Bonus = employee.Bonus,
+                DepartmentId = employee.DepartmentId 
+            }; 
+        
+        private static Employee DTOToEmployee(EmployeeDTO employeeDTO) =>
+            new Employee 
+            { 
+                EmployeeId = employeeDTO.EmployeeId,
+                FirstName = employeeDTO.FirstName,
+                LastName = employeeDTO.LastName,
+                ManagerId = employeeDTO.ManagerId,
+                Salary = employeeDTO.Salary,
+                Bonus = employeeDTO.Bonus,
+                DepartmentId = employeeDTO.DepartmentId 
+            };
+
+        [HttpGet("Employee")]
+        public async Task<ActionResult<IEnumerable<EmployeeDTO>>> GetEmployee() 
+        { 
+            return await _context.Employees
+                .Select(e => EmployeeToDTO(e))
+                .ToListAsync(); 
+        }
+
+        [HttpGet("Employee/{id}")]
+        public async Task<ActionResult<EmployeeDTO>> GetEmployee(int id) 
+        { 
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null) 
+            { 
+                return NotFound(); 
+            } 
+            return EmployeeToDTO(employee); 
+        }
+
+        [HttpPut("Employee{id}")]
+        public async Task<IActionResult> PutEmployee(int id, EmployeeDTO employeeDTO) 
+        { 
+            if (id != employeeDTO.EmployeeId) 
+            { 
+                return BadRequest();
+            } 
+            _context.Entry(DTOToEmployee(employeeDTO)).State = 
+                EntityState.Modified;
+            try 
+            { 
+                await _context.SaveChangesAsync();
+            } 
+            catch (DbUpdateConcurrencyException) 
+            { 
+                if (!EmployeeExists(id)) 
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpPost("Employee")]
+        public async Task<ActionResult<EmployeeDTO>> PostEmployee(EmployeeDTO employeeDTO)
+        {
+            _context.Employees.Add(DTOToEmployee(employeeDTO));
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetEmployee",
+                new { id = employeeDTO.EmployeeId }, employeeDTO);
+        }
+
+        [HttpDelete("Employee/{id}")]
+        public async Task<ActionResult<EmployeeDTO>> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(employee); 
+            await _context.SaveChangesAsync();
+            return EmployeeToDTO(employee); 
+        }
+    }
+}
